04643d3037ead94ae13e3421f23b6b58909d8a35
Jakub Ossowski <jakub.ossowski654@gmail.com>
2024-05-31 12:03:06 +0200

    init: project setup

---
+++ src/app/StronaGlowna/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button"
+
+export function ButtonDemo() {
+  return <Button>Button</Button>
+}
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24  ">
+
+        <h1 className='text-3xl m-2'>Jakub Ossowski</h1>
+        <h2>Nr albumu: 119200</h2>
+
+        <div className="p-5 m-2">
+          <Button className="m-2" asChild>
+             <Link href="/dziobak">Dziobaki</Link>
+          </Button>
+
+        </div>
+      
+    </main>
+  );
+}
---
+++ src/app/dziobak/page.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button"
+
+export function ButtonDemo() {
+  return <Button>Button</Button>
+}
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Dziobak() {
+  return (
+    <main className="flex flex-col items-center ">
+        <h1 className="m-2 text-2xl">Ornithorhynchus anatinus (dziobak australijski po łacinie)</h1>
+        <Image
+           alt="dziobak"
+           src="/poprostudziobak.jpg"
+           width="800"
+           height="600"
+
+        ></Image>   
+           <Button className="m-2" asChild>
+             <Link href="/StronaGlowna">Strona Głowna</Link>
+           </Button>
+           <Pagination className="m-2">
+             <PaginationContent>
+               <PaginationItem>
+                 <PaginationPrevious href="/drugi_dziobak" />
+               </PaginationItem>
+               <PaginationItem>
+                 <PaginationLink href="dziobak">1</PaginationLink>
+               </PaginationItem>
+               <PaginationItem>
+                 <PaginationLink href="/drugi_dziobak">2</PaginationLink>
+               </PaginationItem>
+               <PaginationItem>
+                 <PaginationEllipsis />
+               </PaginationItem>
+               <PaginationItem>
+                 <PaginationNext href="/drugi_dziobak">2</PaginationNext> 
+               </PaginationItem>
+             </PaginationContent>
+           </Pagination>
+    </main>
+  );
+}
